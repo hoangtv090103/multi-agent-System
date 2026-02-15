fe9dedc15e00b82d419b353ddd3621ffab0fb1be
hoangtv090103 <hoangtv090103@gmail.com>
2026-02-15 23:48:43 +0700

    The Writer Agent

--- agents.py
+++ agents.py
@@ -1,5 +1,6 @@
 from langchain_community.tools.ddg_search import DuckDuckGoSearchRun
-
+from langchain_ollama import ChatOllama
+from langchain_core.prompts import ChatPromptTemplate
 from state import AgentState
 
 def research_node(state: AgentState):
@@ -20,3 +21,33 @@ def research_node(state: AgentState):
     }
 
 
+def write_node(state: AgentState):
+    print("Writer is drafting the post...")
+
+    topic = state.get("topic", "")
+    data = state["research_data"][-1] if state["research_data"] else ""
+
+    llm = ChatOllama(model="granite4:tiny-h", temperature=0.7)
+
+    prompt = ChatPromptTemplate.from_template(
+        """You are a tech blog writer. 
+Write a short, engaging blog post about "{topic}" 
+based ONLY on the following research data:
+
+{data}
+
+Return just the blog post content."""
+    )
+
+    chain = prompt | llm
+    response = chain.invoke(
+        {
+            "topic": topic,
+            "data": data
+        }
+    )
+
+    print("Writing complete.")
+    return {
+        "blog_post": response.content
+    }
